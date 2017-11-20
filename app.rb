322b1c7c33a6ae27dd1bb2482d0963f42c88978b
jjwumaster <jjwumaster@gmail.com>
2017-11-20 18:16:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require 'pry'
 
 class App < Sinatra::Base
   get '/' do
@@ -7,8 +8,13 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text]).text
+    hash = @analyzed_text.split("").inject(Hash.new(0)) do |h, v|
+      h[v] += 1
+      h
+    end
+    @letter = hash.max_by {|k, v| v}[0]
+    @freq = hash.max_by {|k, v| v}[1]
     erb :results
   end
 end
